39897bdad8559883a703f0ea931e6f43717c44c7
millywayne <achiengmillicent35@gmail.com>
2020-08-29 19:15:03 +0300

    added jquery file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+$(document).ready(function () {
+	$(".clickable").click(function () {
+		$("#design-showing").toggle();
+		$("#design-hidden").toggle();
+	});
+});
+jQuery('design').click(function(){
+    alert("")
+  });
 
+  jQuery('p').click(function(){
+    alert("")
+  });
+
+  jQuery('img').click(function(){
+    alert("")
+  });
