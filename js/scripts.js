4b72662c5dc6aa75a2c632b3ea5ad1022fae533d
millywayne <achiengmillicent35@gmail.com>
2020-08-30 12:27:05 +0300

    js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,39 @@
 $(document).ready(function () {
-	$(".clickable").click(function () {
-		$("#design-showing").toggle();
-		$("#design-hidden").toggle();
+	$("design_icon").on("click", function () {
+		$("p.design").removeClass("design");
+		$(this).parent().addClass("design");
 	});
+	$("development").test($(this).text());
+	var category = $(this).text().toLowerCase().replace(" ", " ");
+	if (category == "all-lists") {
+		$("list").fadeIn("slow").removeClass("hidden");
+	} else {
+		$(list).each(function () {
+			if (!$(this).hasClass(category)) {
+				$(this).hide().addClass("hidden");
+			} else {
+				$(this).fadeIn("slow").removeClass("hidden");
+			}
+		});
+	}
+	return false;
 });
-jQuery('design').click(function(){
-    alert("")
-  });
+$("list").on("mouseenter", function () {
+	var title = $(this).children().data("title");
+	var desc = $(this).children().data("desc");
+}
 
-  jQuery('development').click(function(){
-    alert("")
-  });
+if (desc == null) {
+	title = "Delani Studio";
+}
+if (title == null) {
+	title = "";
+}
+$(this).append("<div class='overlay'></div>");
+var overlay = $(this).children(".overlay");
+overlay.html("<h3>" + title + "</h3><p>" + desc + "</p>")
+overlay.fadeIn(800);
+});
+$
 
-  jQuery('product management').click(function(){
-    alert("")
-  });
+});
