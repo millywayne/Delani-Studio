719f132a89f3b4d924a8ae107b5dbcc700d7ee7f
millywayne <achiengmillicent35@gmail.com>
2020-08-31 18:01:28 +0300

    modified html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 	$("#design").click(function () {
-		$(".design").slideUp('500').hide('1000');
-		$("#design").show('500');
+		$(".design").slideUp('1500').hide('1000');
+		$("#design").show('1500');
 	});
 	$("#design").click(function () {
-		$(".design").slideDown('500');
-		$(".design").slideUp('500');
+		$(".design").slideDown('1500');
+		$(".design").slideUp('1500');
 	});
 });
 $(document).ready(function(){
